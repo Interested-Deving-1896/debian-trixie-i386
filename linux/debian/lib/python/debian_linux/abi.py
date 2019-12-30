Interested-Deving-1896/debian-trixie-i386

commit 2ca647807ff9e29b72a403c7b6809846914bf644

debian/lib/python/debian_linux/abi.py: Add support for namespaces
Exported symbols can now be defined to belong to a specific namespace,
and Module.symvers includes this as an additional field between name
and module.

The namespace can be an empty string, so when reading we need to split
fields on '\t' and not the default of one-or-more-whitespace.  We then
also need to separate fields with '\t' when writing an ABI reference.

Namespaces are intended for grouping symbols exported for use by
groups of in-tree modules, and we ought to add support for ignoring
ABI changes on this basis.  For now, just add it as an attribute of
Symbol which is compared when checking for ABI changes.

linux/debian/lib/python/debian_linux/abi.py
@@ -1,6 +1,6 @@
 class Symbol(object):
-    def __init__(self, name, module, version, export):
-        self.name, self.module = name, module
+    def __init__(self, name, namespace, module, version, export):
+        self.name, self.namespace, self.module = name, namespace, module
         self.version, self.export = version, export
 
     def __eq__(self, other):
@@ -11,6 +11,8 @@ class Symbol(object):
         # upgrade time, not compile time, so moving a symbol between
         # modules is not an ABI change.  Compare everything else.
         if self.name != other.name:
+            return False
+        if self.namespace != other.namespace:
             return False
         if self.version != other.version:
             return False
@@ -33,10 +35,10 @@ class Symbols(dict):
 
     def read(self, file):
         for line in file:
-            version, name, module, export = line.strip().split()
-            self[name] = Symbol(name, module, version, export)
+            version, name, namespace, module, export = line.split('\t')
+            self[name] = Symbol(name, namespace, module, version, export)
 
     def write(self, file):
         for s in sorted(self.values(), key=lambda i: i.name):
-            file.write("%s %s %s %s\n" %
-                       (s.version, s.name, s.module, s.export))
+            file.write("%s\t%s\t%s\t%s\n" %
+                       (s.version, s.name, s.namespace, s.module, s.export))

commit 442694691e6d32cb8f07bbda59e922652ec9ed8e

debian/lib/python/debian_linux/abi.py: Add one missing string replacement
While adding the support for namespaces, the format string missed one
string replacement to write out in the ABI reference file and will cause

	Updating ABI for arch amd64, featureset none, flavour amd64: FAILED!
	Traceback (most recent call last):
	  File "debian/bin/abiupdate.py", line 192, in update_flavour
	    self.save_abi(version_abi, abi, arch, featureset, flavour)
	  File "debian/bin/abiupdate.py", line 159, in save_abi
	    symbols.write(open(out, 'w'))
	  File "/src/kernel-team/linux/linux/debian/bin/debian_linux/abi.py", line 44, in write
	    (s.version, s.name, s.namespace, s.module, s.export))
	TypeError: not all arguments converted during string formatting

Fixes: 2ca647807ff9 ("debian/lib/python/debian_linux/abi.py: Add support for namespaces")

linux/debian/lib/python/debian_linux/abi.py
@@ -40,5 +40,5 @@ class Symbols(dict):
 
     def write(self, file):
         for s in sorted(self.values(), key=lambda i: i.name):
-            file.write("%s\t%s\t%s\t%s\n" %
+            file.write("%s\t%s\t%s\t%s\t%s\n" %
                        (s.version, s.name, s.namespace, s.module, s.export))