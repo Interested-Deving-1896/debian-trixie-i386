Interested-Deving-1896/debian-trixie-i386

commit e15661f6840cdbcee1373e72c4060460604dcf6a

Cope with change to Module.symvers file structure
With commit 7b727f1c5394 ("modpost: move the namespace field in
Module.symvers last"), upstream has re-ordered the content of
Module.symvers.  Update our tooling to cope with the new input format, and
to preserve the new format in output.

linux/debian/lib/python/debian_linux/abi.py
@@ -35,10 +35,10 @@ class Symbols(dict):
 
     def read(self, file):
         for line in file:
-            version, name, namespace, module, export = line.strip().split('\t')
+            version, name, module, export, namespace = line.strip('\r\n').split('\t')
             self[name] = Symbol(name, namespace, module, version, export)
 
     def write(self, file):
         for s in sorted(self.values(), key=lambda i: i.name):
             file.write("%s\t%s\t%s\t%s\t%s\n" %
-                       (s.version, s.name, s.namespace, s.module, s.export))
+                       (s.version, s.name, s.module, s.export, s.namespace))

commit e466a2e1278c5ed64b410ff72b021691f6c74631

Fix pycodestyle error
e15661f6840c ("Cope with change to Module.symvers file structure") introduced a
pycodestyle error due to a line length >80 columns.

linux/debian/lib/python/debian_linux/abi.py
@@ -35,7 +35,8 @@ class Symbols(dict):
 
     def read(self, file):
         for line in file:
-            version, name, module, export, namespace = line.strip('\r\n').split('\t')
+            version, name, module, export, namespace = \
+                line.strip('\r\n').split('\t')
             self[name] = Symbol(name, namespace, module, version, export)
 
     def write(self, file):