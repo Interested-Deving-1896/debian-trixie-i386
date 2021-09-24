Interested-Deving-1896/debian-trixie-i386

commit 798f3c4f5e23e89ef6f4caa0da6ccc645e1c955b

Allow to override top-level config
Including kernelarch-X configs is kind of a special case.  This config
was dropped if someone ever wanted to override config files for the
top-level config.

linux/debian/bin/gencontrol.py
@@ -608,9 +608,9 @@ class Gencontrol(Base):
             return check_config_files(configs)
 
         kconfig = check_config('config', True)
-        kconfig.extend(check_config("kernelarch-%s/config" %
-                                    config_entry_base['kernel-arch'],
-                                    False))
+        # XXX: We have no way to override kernelarch-X configs
+        kconfig.extend(check_config_default(False,
+            "kernelarch-%s/config" % config_entry_base['kernel-arch']))
         kconfig.extend(check_config("%s/config" % arch, True, arch))
         kconfig.extend(check_config("%s/config.%s" % (arch, flavour), False,
                                     arch, None, flavour))

commit 6c017ef2b925e0c9a4efb72f1811e1e6d9bf771c

debian/bin/gencontrol.py: Fix under-indented line triggering pycodestyle problem
Fixes: 798f3c4f5e23 ("Allow to override top-level config")
Signed-off-by: Salvatore Bonaccorso <carnil@debian.org>

linux/debian/bin/gencontrol.py
@@ -610,7 +610,7 @@ class Gencontrol(Base):
         kconfig = check_config('config', True)
         # XXX: We have no way to override kernelarch-X configs
         kconfig.extend(check_config_default(False,
-            "kernelarch-%s/config" % config_entry_base['kernel-arch']))
+                       "kernelarch-%s/config" % config_entry_base['kernel-arch']))
         kconfig.extend(check_config("%s/config" % arch, True, arch))
         kconfig.extend(check_config("%s/config.%s" % (arch, flavour), False,
                                     arch, None, flavour))