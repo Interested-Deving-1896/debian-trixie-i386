Interested-Deving-1896/debian-trixie-i386

commit 81cbb7bf2ed85d3e08e02ddc4b1b1c3c23f21695

Use new TOML config

linux/debian/bin/buildcheck.py
@@ -4,7 +4,6 @@ import sys
 import glob
 import os
 
-from debian_linux.config import ConfigCoreDump
 from debian_linux.debian import Changelog, VersionLinux
 
 
@@ -96,12 +95,13 @@ class Main(object):
     def __init__(self, dir, arch, featureset, flavour):
         self.args = dir, arch, featureset, flavour
 
-        self.config = ConfigCoreDump(open("debian/config.defines.dump", "rb"))
+        # TODO
+        # self.config = ConfigCoreDump(open("debian/config.defines.dump", "rb"))
 
     def __call__(self):
         fail = 0
 
-        for c in (CheckImage, ):
+        for c in ():
             fail |= c(self.config, *self.args)(sys.stdout)
 
         return fail

linux/debian/.gitignore
@@ -21,7 +21,6 @@
 /*.debhelper
 /*.maintscript
 /*.substvars
-/config.defines.dump
 /control
 /control.*
 /files

linux/debian/README.source
@@ -114,30 +114,30 @@ number of files under debian/config and (if it exists)
 debian/config.local.  They are read in the following order, such that
 files later on the list can override settings from earlier files.
 Files in debian/config.local can also override settings from the
-corresponding file in debian/config.  Most of the files are optional
-and the filenames can generally be overridden by explicit lists
-(possibly empty) specified in the 'defines' files.
+corresponding file in debian/config.
 
 1. Common:
    - Default filename: config
-   - Filename list: [image]configs in defines
+   - Filename list: [build.config]
 2. Per kernel architecture:
-   - Filename: kernelarch-<karch>/config (optional)
-3. Per architecture:
+   - Default filename: kernelarch-<karch>/config
+   - Filename list: [kernelarch.build.config]
+3. Per Debian architecture:
    - Default filename: <arch>/config
-   - Filename list: [image]configs in <arch>/defines
-4. Per architecture and flavour:
-   - Default filename: <arch>/config.<flavour> (optional)
-   - Filename list: [<flavour>_image]configs in <arch>/defines
+   - Filename list: [kernelarch.debianarch.build.config]
+4. Per Debian architecture and flavour:
+   - Default filename: <arch>/config.<flavour>
+   - Filename list: [kernelarch.debianarch.flavour.build.config]
 5. Per featureset:
-   - Default filename: featureset-<fset>/config (optional)
-   - Filename list: [image]configs in featureset-<fset>/defines
-6. Per architecture and featureset:
-   - Default filename: <arch>/<fset>/config (optional)
-   - Filename list: [image]configs in <arch>/<fset>/defines
-7. Per architecture, featureset, and flavour:
-   - Default filename: <arch>/<fset>/config.<flavour> (optional)
-   - Filename list: [<flavour>_image]configs in <arch>/<fset>/defines
+   - Default filename: featureset-<fset>/config
+   - Filename list: [featureset.build.config]
+6. Per Debian architecture and featureset:
+   - Default filename: <arch>/<fset>/config
+   - Filename list: [kernelarch.debianarch.featureset.build.config]
+7. Per Debian architecture, featureset, and flavour:
+   - Default filename: <arch>/<fset>/config.<flavour>
+   - Filename list:
+     [kernelarch.debianarch.featureset.flavour.build.config]
 
 You can check the final list of configuration files by reading
 debian/rules.gen.  Each binary-arch_<arch>_<fset>_<flavour>_image
@@ -159,7 +159,7 @@ The master control file debian/control must be generated before
 the package is uploaded. debian/rules contains the debian/control 
 target, which generates the control file by invoking the 
 debian/bin/gencontrol.py script, which combines the templates from
-the templates directory and architecture-specific defines file to
+the templates directory and architecture-specific defines.toml file to
 produce the debian/control file. Note that this target is intentionally
 made to fail with a non-zero exit code to make sure that it is never
 run during an automatic build. The following variables are substituted
@@ -179,7 +179,7 @@ into the templates:
                -image descriptions.
 
 Normally, the arch-specific contents should be controlled by
-adjusting the corresponding defines file.
+adjusting the corresponding defines.toml file.
 
 Build-dependencies that relate to specific binary packages can be
 specified in a Build-Depends field in the template for that binary

linux/debian/lib/python/debian_linux/gencontrol.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import contextlib
+import itertools
 import pathlib
 import re
 from collections import OrderedDict
@@ -14,6 +15,12 @@ from typing import (
     IO,
 )
 
+from .config_v2 import (
+    ConfigMerged,
+    ConfigMergedDebianarch,
+    ConfigMergedFeatureset,
+    ConfigMergedFlavour,
+)
 from .debian import Changelog, PackageArchitecture, \
     Version, _ControlFileDict
 from .utils import Templates
@@ -365,31 +372,12 @@ class PackagesBundle:
             f.write('\n')
 
 
-def iter_featuresets(config) -> Iterable[str]:
-    for featureset in config['base', ]['featuresets']:
-        if config.merge('base', None, featureset).get('enabled', True):
-            yield featureset
-
-
-def iter_arches(config) -> Iterable[str]:
-    return iter(config['base', ]['arches'])
-
-
-def iter_arch_featuresets(config, arch) -> Iterable[str]:
-    for featureset in config['base', arch].get('featuresets', []):
-        if config.merge('base', arch, featureset).get('enabled', True):
-            yield featureset
-
-
-def iter_flavours(config, arch, featureset) -> Iterable[str]:
-    return iter(config['base', arch, featureset]['flavours'])
-
-
 class Gencontrol(object):
+    config: ConfigMerged
     vars: dict[str, str]
     bundles: dict[str, PackagesBundle]
 
-    def __init__(self, config, templates, version=Version) -> None:
+    def __init__(self, config: ConfigMerged, templates, version=Version) -> None:
         self.config, self.templates = config, templates
         self.changelog = Changelog(version=version)
         self.vars = {}
@@ -417,27 +405,53 @@ class Gencontrol(object):
 
         makeflags = MakeFlags()
 
-        self.do_main_setup(vars, makeflags)
-        self.do_main_makefile(makeflags)
-        self.do_main_packages(vars, makeflags)
-        self.do_main_recurse(vars, makeflags)
+        self.do_main_setup(self.config, vars, makeflags)
+        self.do_main_makefile(self.config, vars, makeflags)
+        self.do_main_packages(self.config, vars, makeflags)
+        self.do_main_recurse(self.config, vars, makeflags)
 
-    def do_main_setup(self, vars, makeflags) -> None:
+    def do_main_setup(
+        self,
+        config: ConfigMerged,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_main_makefile(self, makeflags) -> None:
+    def do_main_makefile(
+        self,
+        config: ConfigMerged,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_main_packages(self, vars, makeflags) -> None:
+    def do_main_packages(
+        self,
+        config: ConfigMerged,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_main_recurse(self, vars, makeflags) -> None:
-        for featureset in iter_featuresets(self.config):
-            self.do_indep_featureset(featureset,
-                                     vars.copy(), makeflags.copy())
-        for arch in iter_arches(self.config):
-            self.do_arch(arch, vars.copy(),
-                         makeflags.copy())
+    def do_main_recurse(
+        self,
+        config: ConfigMerged,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        for featureset in config.root_featuresets:
+            self.do_indep_featureset(featureset, vars.copy(), makeflags.copy())
+
+        # Sort the output the same way as before
+        for arch in sorted(
+            itertools.chain.from_iterable(
+                i.debianarchs for i in config.kernelarchs
+            ),
+            key=lambda i: i.name
+        ):
+            if arch.enable:
+                self.do_arch(arch, vars.copy(), makeflags.copy())
 
     def do_extra(self) -> None:
         try:
@@ -456,81 +470,158 @@ class Gencontrol(object):
             self.bundle.add_packages(packages_extra, (arch, ),
                                      MakeFlags(), check_packages=False)
 
-    def do_indep_featureset(self, featureset, vars, makeflags) -> None:
+    def do_indep_featureset(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         vars['localversion'] = ''
-        if featureset != 'none':
-            vars['localversion'] = '-' + featureset
+        if config.name_featureset != 'none':
+            vars['localversion'] = '-' + config.name_featureset
 
-        self.do_indep_featureset_setup(vars, makeflags, featureset)
-        self.do_indep_featureset_makefile(featureset, makeflags)
-        self.do_indep_featureset_packages(featureset,
-                                          vars, makeflags)
+        self.do_indep_featureset_setup(config, vars, makeflags)
+        self.do_indep_featureset_makefile(config, vars, makeflags)
+        self.do_indep_featureset_packages(config, vars, makeflags)
 
-    def do_indep_featureset_setup(self, vars, makeflags, featureset) -> None:
+    def do_indep_featureset_setup(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_indep_featureset_makefile(self, featureset, makeflags) -> None:
-        makeflags['FEATURESET'] = featureset
+    def do_indep_featureset_makefile(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        makeflags['FEATURESET'] = config.name
 
-    def do_indep_featureset_packages(self, featureset, vars, makeflags) -> None:
+    def do_indep_featureset_packages(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_arch(self, arch, vars, makeflags) -> None:
-        vars['arch'] = arch
+    def do_arch(
+        self,
+        config: ConfigMergedDebianarch,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        vars['arch'] = config.name
 
-        self.do_arch_setup(vars, makeflags, arch)
-        self.do_arch_makefile(arch, makeflags)
-        self.do_arch_packages(arch, vars, makeflags)
-        self.do_arch_recurse(arch, vars, makeflags)
+        self.do_arch_setup(config, vars, makeflags)
+        self.do_arch_makefile(config, vars, makeflags)
+        self.do_arch_packages(config, vars, makeflags)
+        self.do_arch_recurse(config, vars, makeflags)
 
-    def do_arch_setup(self, vars, makeflags, arch) -> None:
+    def do_arch_setup(
+        self,
+        config: ConfigMergedDebianarch,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_arch_makefile(self, arch, makeflags) -> None:
-        makeflags['ARCH'] = arch
+    def do_arch_makefile(
+        self,
+        config: ConfigMergedDebianarch,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        makeflags['ARCH'] = config.name
 
-    def do_arch_packages(self, arch, vars, makeflags) -> None:
+    def do_arch_packages(
+        self,
+        config: ConfigMergedDebianarch,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_arch_recurse(self, arch, vars, makeflags) -> None:
-        for featureset in iter_arch_featuresets(self.config, arch):
-            self.do_featureset(arch, featureset,
-                               vars.copy(), makeflags.copy())
+    def do_arch_recurse(
+        self,
+        config: ConfigMergedDebianarch,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        for featureset in config.featuresets:
+            if featureset.enable:
+                self.do_featureset(featureset, vars.copy(), makeflags.copy())
 
-    def do_featureset(self, arch, featureset, vars, makeflags) -> None:
+    def do_featureset(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         vars['localversion'] = ''
-        if featureset != 'none':
-            vars['localversion'] = '-' + featureset
+        if config.name_featureset != 'none':
+            vars['localversion'] = '-' + config.name_featureset
 
-        self.do_featureset_setup(vars, makeflags, arch, featureset)
-        self.do_featureset_makefile(arch, featureset, makeflags)
-        self.do_featureset_packages(arch, featureset, vars, makeflags)
-        self.do_featureset_recurse(arch, featureset, vars, makeflags)
+        self.do_featureset_setup(config, vars, makeflags)
+        self.do_featureset_makefile(config, vars, makeflags)
+        self.do_featureset_packages(config, vars, makeflags)
+        self.do_featureset_recurse(config, vars, makeflags)
 
-    def do_featureset_setup(self, vars, makeflags, arch, featureset) -> None:
+    def do_featureset_setup(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_featureset_makefile(self, arch, featureset, makeflags) -> None:
-        makeflags['FEATURESET'] = featureset
+    def do_featureset_makefile(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        makeflags['FEATURESET'] = config.name
 
-    def do_featureset_packages(self, arch, featureset, vars, makeflags) -> None:
+    def do_featureset_packages(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
-    def do_featureset_recurse(self, arch, featureset, vars, makeflags) -> None:
-        for flavour in iter_flavours(self.config, arch, featureset):
-            self.do_flavour(arch, featureset, flavour,
-                            vars.copy(), makeflags.copy())
+    def do_featureset_recurse(
+        self,
+        config: ConfigMergedFeatureset,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        for flavour in config.flavours:
+            if flavour.enable:
+                self.do_flavour(flavour, vars.copy(), makeflags.copy())
 
-    def do_flavour(self, arch, featureset, flavour, vars,
-                   makeflags):
-        vars['localversion'] += '-' + flavour
+    def do_flavour(
+        self,
+        config: ConfigMergedFlavour,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        vars['localversion'] += '-' + config.name_flavour
 
-        self.do_flavour_setup(vars, makeflags, arch, featureset, flavour)
-        self.do_flavour_makefile(arch, featureset, flavour, makeflags)
-        self.do_flavour_packages(arch, featureset, flavour,
-                                 vars, makeflags)
+        self.do_flavour_setup(config, vars, makeflags)
+        self.do_flavour_makefile(config, vars, makeflags)
+        self.do_flavour_packages(config, vars, makeflags)
 
-    def do_flavour_setup(self, vars, makeflags, arch, featureset, flavour) -> None:
+    def do_flavour_setup(
+        self,
+        config: ConfigMergedFlavour,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         for i in (
             ('kernel-arch', 'KERNEL_ARCH'),
             ('localversion', 'LOCALVERSION'),
@@ -538,10 +629,20 @@ class Gencontrol(object):
             if i[0] in vars:
                 makeflags[i[1]] = vars[i[0]]
 
-    def do_flavour_makefile(self, arch, featureset, flavour, makeflags) -> None:
-        makeflags['FLAVOUR'] = flavour
+    def do_flavour_makefile(
+        self,
+        config: ConfigMergedFlavour,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
+        makeflags['FLAVOUR'] = config.name
 
-    def do_flavour_packages(self, arch, featureset, flavour, vars, makeflags) -> None:
+    def do_flavour_packages(
+        self,
+        config: ConfigMergedFlavour,
+        vars: dict[str, str],
+        makeflags: MakeFlags,
+    ) -> None:
         pass
 
     def substitute(self, s: str | list | tuple, vars) -> str | list:

linux/debian/rules
@@ -98,7 +98,6 @@ maintainerclean:
 # We cannot use dh_clean here because it requires debian/control to exist
 	rm -rf debian/.debhelper debian/*.debhelper* debian/files debian/generated.*
 	rm -f \
-		debian/config.defines.dump \
 		debian/control \
 		debian/control.md5sum \
 		debian/linux-doc.maintscript \
@@ -123,7 +122,7 @@ clean: debian/control
 	dh_clean
 
 CONTROL_FILES = $(BUILD_DIR)/version-info $(wildcard debian/templates/*.in)
-CONTROL_FILES += debian/config/defines $(wildcard debian/config/*/defines) $(wildcard debian/config/*/*/defines)
+CONTROL_FILES += debian/config/defines.toml $(wildcard debian/config/*/defines.toml)
 CONTROL_FILES += debian/installer/package-list
 
 # debian/bin/gencontrol.py uses debian/changelog as input, but the

linux/debian/rules.real
@@ -366,7 +366,6 @@ binary_support: PACKAGE_ROOT = /usr/share/$(PACKAGE_NAME)
 binary_support:
 	$(dh_binary_pre)
 	dh_installdirs $(PACKAGE_ROOT)/lib/python/debian_linux $(PACKAGE_ROOT)/modules
-	cp debian/config.defines.dump $(DESTDIR)$(PACKAGE_ROOT)
 	cp -R debian/installer $(DESTDIR)$(PACKAGE_ROOT)/installer
 	cp debian/lib/python/debian_linux/*.py $(DESTDIR)$(PACKAGE_ROOT)/lib/python/debian_linux
 	dh_python3

commit 82fdce43d60ce3c8b930425bf62e826f66338fa4

d/b/buildcheck.py: Restore reading of config to pass to check classes
This was left broken after switching the config format to TOML.

Fixes: 81cbb7bf2ed8 ("Use new TOML config")

linux/debian/bin/buildcheck.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
 
+import itertools
+import pathlib
 import sys
+
+from debian_linux.config_v2 import Config
 
 
 class Main(object):
     def __init__(self, dir, arch, featureset, flavour):
         self.args = dir, arch, featureset, flavour
+
+        config_dirs = [
+            pathlib.Path('debian/config'),
+            pathlib.Path('debian/config.local'),
+        ]
+        top_config = Config.read_orig(config_dirs).merged
+        arch_config = next(
+            ac
+            for ac in itertools.chain.from_iterable(
+                kac.debianarchs for kac in top_config.kernelarchs)
+            if ac.name == arch
+        )
+        fs_config = next(fsc for fsc in arch_config.featuresets
+                         if fsc.name == featureset)
+        self.config = next(fc for fc in fs_config.flavours
+                           if fc.name == flavour)
 
     def __call__(self):
         fail = 0