Interested-Deving-1896/debian-trixie-i386

commit ae6ab60b1832a436ad209dba9ef0c022b9002924

Convert control entries into dataclasses

linux/debian/bin/gencontrol.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import dataclasses
 import json
 import locale
 import os
@@ -19,7 +20,9 @@ from debian_linux.config_v2 import (
     ConfigMergedFeatureset,
     ConfigMergedFlavour,
 )
+from debian_linux.dataclasses_deb822 import read_deb822, write_deb822
 from debian_linux.debian import \
+    PackageBuildprofile, \
     PackageRelationEntry, PackageRelationGroup, \
     VersionLinux, BinaryPackage
 from debian_linux.gencontrol import Gencontrol as Base, PackagesBundle, \
@@ -86,7 +89,7 @@ class Gencontrol(Base):
         makeflags['SOURCE_SUFFIX'] = vars['source_suffix']
 
         # Prepare to generate debian/tests/control
-        self.tests_control = self.templates.get_tests_control('main.tests-control', vars)
+        self.tests_control = list(self.templates.get_tests_control('main.tests-control', vars))
 
     def do_main_makefile(
         self,
@@ -145,9 +148,9 @@ class Gencontrol(Base):
             libcdev_makeflags['ALL_LIBCDEV_MULTIARCHES'] = ' '.join(sorted(libcdev_multiarches))
 
             for package in self.bundle.add('libc-dev', (), libcdev_makeflags, vars):
-                package.setdefault('Provides').extend([
+                package.provides.extend([
                     PackageRelationGroup(
-                        f'{package["Package"]}-{arch}-cross (= ${{binary:Version}})'
+                        f'{package.name}-{arch}-cross (= ${{binary:Version}})'
                     )
                     for arch in sorted(libcdev_debianarches)
                 ])
@@ -212,7 +215,7 @@ class Gencontrol(Base):
             bundle_signed = self.bundles[f'signed-{arch}'] = \
                 PackagesBundle(f'signed-{arch}', self.templates)
             bundle_signed.packages['source'] = \
-                self.templates.get_source_control('signed.source.control', vars)[0]
+                list(self.templates.get_source_control('signed.source.control', vars))[0]
 
             with bundle_signed.open('source/lintian-overrides', 'w') as f:
                 f.write(self.substitute(
@@ -294,7 +297,7 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
 
         # Generate compiler build-depends for native:
         # gcc-13 [arm64] <!cross !pkg.linux.nokernel>
-        self.bundle.packages['source']['Build-Depends-Arch'].merge([
+        self.bundle.packages['source'].build_depends_arch.merge([
             PackageRelationEntry(
                 relation_compiler,
                 arches={arch},
@@ -304,7 +307,7 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
 
         # Generate compiler build-depends for cross:
         # gcc-13-aarch64-linux-gnu [arm64] <cross !pkg.linux.nokernel>
-        self.bundle.packages['source']['Build-Depends-Arch'].merge([
+        self.bundle.packages['source'].build_depends_arch.merge([
             PackageRelationEntry(
                 relation_compiler,
                 name=f'{relation_compiler.name}-{config.defs_debianarch.gnutype_package}',
@@ -317,7 +320,7 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
         # gcc-13-hppa64-linux-gnu [hppa] <!pkg.linux.nokernel>
         if gnutype := config.build.compiler_gnutype:
             if gnutype != config.defs_debianarch.gnutype:
-                self.bundle.packages['source']['Build-Depends-Arch'].merge([
+                self.bundle.packages['source'].build_depends_arch.merge([
                     PackageRelationEntry(
                         relation_compiler,
                         name=f'{relation_compiler.name}-{gnutype.replace("_", "-")}',
@@ -331,7 +334,7 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
         # XXX: Linux uses various definitions for this, all ending with "gcc", not $CC
         if gnutype := config.build.compiler_gnutype_compat:
             if gnutype != config.defs_debianarch.gnutype:
-                self.bundle.packages['source']['Build-Depends-Arch'].merge([
+                self.bundle.packages['source'].build_depends_arch.merge([
                     PackageRelationEntry(
                         f'gcc-{gnutype.replace("_", "-")}',
                         arches={arch},
@@ -371,7 +374,7 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
                       'Conflicts', 'Breaks'):
             for i in getattr(config.relations.image, field.lower(), []):
                 for package_image in packages_image:
-                    package_image.setdefault(field).merge(
+                    getattr(package_image, field.lower()).merge(
                         PackageRelationGroup(i, arches={arch})
                     )
 
@@ -382,7 +385,7 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
                     if entry.operator is not None:
                         entry.operator = -entry.operator
                         for package_image in packages_image:
-                            package_image.setdefault('Breaks').append(PackageRelationGroup([entry]))
+                            package_image.breaks.append(PackageRelationGroup([entry]))
 
         if desc_parts := config.description.parts:
             # XXX: Workaround, we need to support multiple entries of the same
@@ -390,12 +393,12 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
             parts = list(set(desc_parts))
             parts.sort()
             for package_image in packages_image:
-                desc = package_image['Description']
+                desc = package_image.description
                 for part in parts:
                     desc.append(config.description.long[part])
                     desc.append_short(config.description.short[part])
 
-        packages_headers[0]['Depends'].merge(relation_compiler_header)
+        packages_headers[0].depends.merge(relation_compiler_header)
         packages_own.extend(packages_image)
         packages_own.extend(packages_headers)
 
@@ -420,10 +423,8 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
                 config.defs_flavour.is_default
                 and not self.vars['source_suffix']
             ):
-                packages_meta[0].setdefault('Provides') \
-                                .append('linux-image-generic')
-                packages_meta[1].setdefault('Provides') \
-                                .append('linux-headers-generic')
+                packages_meta[0].provides.append('linux-image-generic')
+                packages_meta[1].provides.append('linux-headers-generic')
 
             packages_own.extend(packages_meta)
 
@@ -450,19 +451,19 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
         # In a quick build, only build the quick flavour (if any).
         if not config.defs_flavour.is_quick:
             for package in packages_own:
-                package['Build-Profiles'][0].neg.add('pkg.linux.quick')
+                package.build_profiles[0].neg.add('pkg.linux.quick')
 
         tests_control_image = self.templates.get_tests_control('image.tests-control', vars)
         for c in tests_control_image:
-            c.setdefault('Depends').extend(
-                [i['Package'] for i in packages_image_unsigned]
+            c.depends.extend(
+                [i.name for i in packages_image_unsigned]
             )
 
         tests_control_headers = self.templates.get_tests_control('headers.tests-control', vars)
         for c in tests_control_headers:
-            c.setdefault('Depends').extend(
-                [i['Package'] for i in packages_headers] +
-                [i['Package'] for i in packages_image_unsigned]
+            c.depends.extend(
+                [i.name for i in packages_headers] +
+                [i.name for i in packages_image_unsigned]
             )
 
         self.tests_control.extend(tests_control_image)
@@ -508,23 +509,27 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
                     stdout=subprocess.PIPE,
                     text=True,
                     env=kw_env)
-                udeb_packages_base = BinaryPackage.read_rfc822(kw_proc.stdout)
+                assert kw_proc.stdout is not None
+                udeb_packages_base = list(read_deb822(BinaryPackage, kw_proc.stdout))
                 kw_proc.wait()
                 if kw_proc.returncode != 0:
                     raise RuntimeError('kernel-wedge exited with code %d' %
                                        kw_proc.returncode)
 
-            udeb_packages = []
-            for package_base in udeb_packages_base:
-                package = package_base.copy()
-                # kernel-wedge currently chokes on Build-Profiles so add it now
-                package['Build-Profiles'] = (
-                    '<!noudeb !pkg.linux.nokernel !pkg.linux.quick>')
-                package.meta['rules-target'] = 'installer'
-                udeb_packages.append(package)
+            udeb_packages = [
+                dataclasses.replace(
+                    package_base,
+                    # kernel-wedge currently chokes on Build-Profiles so add it now
+                    build_profiles=PackageBuildprofile.parse(
+                        '<!noudeb !pkg.linux.nokernel !pkg.linux.quick>',
+                    ),
+                    meta_rules_target='installer',
+                )
+                for package_base in udeb_packages_base
+            ]
 
             makeflags_local = makeflags.copy()
-            makeflags_local['IMAGE_PACKAGE_NAME'] = udeb_packages[0]['Package']
+            makeflags_local['IMAGE_PACKAGE_NAME'] = udeb_packages[0].name
 
             bundle_signed.add_packages(
                 udeb_packages,
@@ -533,24 +538,27 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
             )
 
             if build_signed:
-                udeb_packages = []
                 # XXX This is a hack to exclude the udebs from
                 # the package list while still being able to
                 # convince debhelper and kernel-wedge to go
                 # part way to building them.
-                for package_base in udeb_packages_base:
-                    package = package_base.copy()
-                    # kernel-wedge currently chokes on Build-Profiles so add it now
-                    package['Build-Profiles'] = (
-                        '<pkg.linux.udeb-unsigned-test-build !noudeb'
-                        ' !pkg.linux.nokernel !pkg.linux.quick>')
-                    package.meta['rules-target'] = 'installer-test'
-                    udeb_packages.append(package)
+                udeb_packages = [
+                    dataclasses.replace(
+                        package_base,
+                        # kernel-wedge currently chokes on Build-Profiles so add it now
+                        build_profiles=PackageBuildprofile.parse(
+                            '<pkg.linux.udeb-unsigned-test-build !noudeb'
+                            ' !pkg.linux.nokernel !pkg.linux.quick>',
+                        ),
+                        meta_rules_target='installer-test',
+                    )
+                    for package_base in udeb_packages_base
+                ]
 
                 self.bundle.add_packages(
                     udeb_packages,
                     (config.name_debianarch, config.name_featureset, config.name_flavour),
-                    makeflags, arch=arch, check_packages=False,
+                    makeflags_local, arch=arch, check_packages=False,
                 )
 
     def process_changelog(self) -> None:
@@ -609,7 +617,10 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
             pkg_sign_entries = {}
 
             for p in bundle.packages.values():
-                if pkg_sign_pkg := p.meta.get('sign-package'):
+                if not isinstance(p, BinaryPackage):
+                    continue
+
+                if pkg_sign_pkg := p.meta_sign_package:
                     pkg_sign_entries[pkg_sign_pkg] = {
                         'trusted_certs': [],
                         'files': [
@@ -617,7 +628,7 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
                                 'sig_type': e.split(':', 1)[-1],
                                 'file': e.split(':', 1)[0],
                             }
-                            for e in p.meta['sign-files'].split()
+                            for e in p.meta_sign_files
                         ],
                     }
 
@@ -626,8 +637,8 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
                     json.dump({'packages': pkg_sign_entries}, f, indent=2)
 
     def write_tests_control(self) -> None:
-        self.bundle.write_rfc822(open("debian/tests/control", 'w'),
-                                 self.tests_control)
+        with open("debian/tests/control", 'w') as f:
+            write_deb822(self.tests_control, f)
 
 
 if __name__ == '__main__':

linux/debian/lib/python/debian_linux/gencontrol.py
@@ -21,8 +21,9 @@ from .config_v2 import (
     ConfigMergedFeatureset,
     ConfigMergedFlavour,
 )
+from .dataclasses_deb822 import write_deb822
 from .debian import Changelog, PackageArchitecture, \
-    Version, _ControlFileDict
+    Version, BinaryPackage
 from .utils import Templates
 
 
@@ -35,7 +36,7 @@ class PackagesList(OrderedDict):
             self[package['Package']] = package
 
     def setdefault(self, package) -> Any:
-        return super().setdefault(package['Package'], package)
+        return super().setdefault(package.name, package)
 
 
 class Makefile:
@@ -180,13 +181,13 @@ class PackagesBundle:
         ret = []
         for raw_package in self.templates.get_control(f'{pkgid}.control', replace):
             package = self.packages.setdefault(raw_package)
-            package_name = package['Package']
+            package_name = package.name
             ret.append(package)
 
-            package.meta.setdefault('rules-ruleids', {})[ruleid] = makeflags
+            package.meta_rules_ruleids[ruleid] = makeflags
             if arch:
-                package.meta.setdefault('architectures', PackageArchitecture()).add(arch)
-            package.meta['rules-check-packages'] = check_packages
+                package.meta_architectures.add(arch)
+            package.meta_rules_check_packages = check_packages
 
             for name in (
                     'NEWS',
@@ -211,7 +212,7 @@ class PackagesBundle:
 
     def add_packages(
             self,
-            packages: Iterable[_ControlFileDict],
+            packages: Iterable[BinaryPackage],
             ruleid: Iterable[str],
             makeflags: MakeFlags,
             *,
@@ -220,10 +221,10 @@ class PackagesBundle:
     ) -> None:
         for package in packages:
             package = self.packages.setdefault(package)
-            package.meta.setdefault('rules-ruleids', {})[ruleid] = makeflags
+            package.meta_rules_ruleids[ruleid] = makeflags
             if arch:
-                package.meta.setdefault('architectures', PackageArchitecture()).add(arch)
-            package.meta['rules-check-packages'] = check_packages
+                package.meta_architectures.add(arch)
+            package.meta_rules_check_packages = check_packages
 
     def path(self, name) -> pathlib.Path:
         if self.name:
@@ -262,20 +263,19 @@ class PackagesBundle:
         targets: dict[frozenset[str], dict] = {}
 
         for package_name, package in self.packages.items():
-            target_name = package.meta.get('rules-target')
-            ruleids = package.meta.get('rules-ruleids')
-            makeflags = MakeFlags({
-                # Requires Python 3.9+
-                k.removeprefix('rules-makeflags-').upper(): v
-                for (k, v) in package.meta.items() if k.startswith('rules-makeflags-')
-            })
+            if not isinstance(package, BinaryPackage):
+                continue
+
+            target_name = package.meta_rules_target
+            ruleids = package.meta_rules_ruleids
+            makeflags = MakeFlags(package.meta_rules_makeflags)
 
             if ruleids:
-                arches = package.meta.get('architectures')
+                arches = package.meta_architectures
                 if arches:
-                    package['Architecture'] = arches
+                    package.architecture = arches
                 else:
-                    arches = package.get('Architecture')
+                    arches = package.architecture
 
                 if target_name:
                     for ruleid, makeflags_package in ruleids.items():
@@ -291,7 +291,7 @@ class PackagesBundle:
                             },
                         )
 
-                        if package.meta['rules-check-packages']:
+                        if package.meta_rules_check_packages:
                             target.setdefault('packages', set()).add(package_name)
                         else:
                             target.setdefault('packages_extra', set()).add(package_name)
@@ -336,21 +336,21 @@ class PackagesBundle:
         for name, package in self.packages.items():
             if name == "source":
                 continue
-            dep = package.get("Build-Depends")
+            dep = package.build_depends
             if not dep:
                 continue
-            del package["Build-Depends"]
-            if package["Architecture"] == arch_all:
-                dep_type = "Build-Depends-Indep"
+            package.build_depends = None
+            if package.architecture == arch_all:
+                dep_type = "build_depends_indep"
             else:
-                dep_type = "Build-Depends-Arch"
+                dep_type = "build_depends_arch"
             for group in dep:
                 for item in group:
-                    if package["Architecture"] != arch_all and not item.arches:
-                        item.arches = package["Architecture"]
-                    if package.get("Build-Profiles") and not item.restrictions:
-                        item.restrictions = package["Build-Profiles"]
-                source.setdefault(dep_type).merge(group)
+                    if package.architecture != arch_all and not item.arches:
+                        item.arches = package.architecture
+                    if package.build_profiles and not item.restrictions:
+                        item.restrictions = package.build_profiles
+                getattr(source, dep_type).merge(group)
 
     def write(self) -> None:
         self.write_control()
@@ -358,18 +358,11 @@ class PackagesBundle:
 
     def write_control(self) -> None:
         with self.open('control') as f:
-            self.write_rfc822(f, self.packages.values())
+            write_deb822(self.packages.values(), f)
 
     def write_makefile(self) -> None:
         with self.open('rules.gen') as f:
             self.makefile.write(f)
-
-    def write_rfc822(self, f: IO, entries: Iterable) -> None:
-        for entry in entries:
-            for key, value in entry.items():
-                if value:
-                    f.write(u"%s: %s\n" % (key, value))
-            f.write('\n')
 
 
 class Gencontrol(object):
@@ -395,9 +388,9 @@ class Gencontrol(object):
         self.write()
 
     def do_source(self) -> None:
-        source = self.templates.get_source_control("source.control", self.vars)[0]
-        if not source.get('Source'):
-            source['Source'] = self.changelog[0].source
+        source = list(self.templates.get_source_control("source.control", self.vars))[0]
+        if not source.name:
+            source.name = self.changelog[0].source
         self.bundle.packages['source'] = source
 
     def do_main(self) -> None:
@@ -645,10 +638,7 @@ class Gencontrol(object):
     ) -> None:
         pass
 
-    def substitute(self, s: str | list | tuple, vars) -> str | list:
-        if isinstance(s, (list, tuple)):
-            return [self.substitute(i, vars) for i in s]
-
+    def substitute(self, s: str, vars) -> str:
         def subst(match) -> str:
             return vars[match.group(1)]
 

linux/debian/lib/python/debian_linux/dataclasses_deb822.py
@@ -1,0 +1,234 @@
+from __future__ import annotations
+
+import dataclasses
+import re
+from typing import (
+    Any,
+    Callable,
+    Generic,
+    IO,
+    Iterable,
+    Optional,
+    overload,
+    TypeVar,
+    TYPE_CHECKING,
+)
+
+_T = TypeVar('_T')
+
+if TYPE_CHECKING:
+    from dataclasses import _DataclassT
+else:
+    # We can only get to _DataclassT during type checking, use a generic type during runtime
+    _DataclassT = _T
+
+__all__ = [
+    'field_deb822',
+    'read_deb822',
+    'write_deb822',
+    'Deb822DecodeError',
+]
+
+
+class Deb822Field(Generic[_T]):
+    key: str
+    load: Optional[Callable[[str], _T]]
+    dump: Optional[Callable[[_T], str]]
+
+    def __init__(
+        self, *,
+        key: str,
+        load: Optional[Callable[[str], _T]],
+        dump: Optional[Callable[[_T], str]],
+    ) -> None:
+        self.key = key
+        self.load = load
+        self.dump = dump
+
+
+# The return type _T is technically wrong, but it allows checking if during
+# runtime we get the correct type.
+@overload
+def field_deb822(
+    deb822_key: str,
+    /, *,
+    deb822_load: Optional[Callable[[str], _T]] = None,
+    deb822_dump: Optional[Callable[[_T], str]] = str,
+    default: _T,
+) -> _T:
+    ...
+
+
+@overload
+def field_deb822(
+    deb822_key: str,
+    /, *,
+    deb822_load: Optional[Callable[[str], _T]] = None,
+    deb822_dump: Optional[Callable[[_T], str]] = str,
+    default_factory: Callable[[], _T],
+) -> _T:
+    ...
+
+
+@overload
+def field_deb822(
+    deb822_key: str,
+    /, *,
+    deb822_load: Optional[Callable[[str], _T]] = None,
+    deb822_dump: Optional[Callable[[_T], str]] = str,
+) -> _T:
+    ...
+
+
+def field_deb822(
+    deb822_key: str,
+    /, *,
+    deb822_load: Optional[Callable[[str], _T]] = None,
+    deb822_dump: Optional[Callable[[_T], str]] = str,
+    default: Any = dataclasses.MISSING,
+    default_factory: Any = dataclasses.MISSING,
+) -> Any:
+    metadata: dict[str, Any] = {
+        'deb822': Deb822Field(
+            key=deb822_key,
+            load=deb822_load,
+            dump=deb822_dump,
+        ),
+    }
+
+    if default is not dataclasses.MISSING:
+        return dataclasses.field(
+            default=default,
+            metadata=metadata,
+        )
+    else:
+        return dataclasses.field(
+            default_factory=default_factory,
+            metadata=metadata,
+        )
+
+
+class Deb822DecodeError(ValueError):
+    pass
+
+
+class Deb822DecodeState(Generic[_DataclassT]):
+    cls: type[_DataclassT]
+    fields: dict[str, dataclasses.Field]
+    ignore_unknown: bool
+
+    data: dict[Optional[dataclasses.Field], str]
+    current: Optional[dataclasses.Field]
+
+    _line_re = re.compile(r'''
+        ^
+        (
+            [ \t](?P<cont>.*)
+            |
+            (?P<key>[^: \t\n\r\f\v]+)\s*:\s*(?P<value>.*)
+        )
+        $
+    ''', re.VERBOSE)
+
+    def __init__(
+        self,
+        cls: type[_DataclassT],
+        ignore_unknown: bool,
+    ) -> None:
+        self.reset()
+
+        self.cls = cls
+        self.fields = {}
+        self.ignore_unknown = ignore_unknown
+
+        for i in dataclasses.fields(cls):
+            if i.init and (deb822_field := i.metadata.get('deb822')):
+                self.fields[deb822_field.key] = i
+
+    def reset(self) -> None:
+        self.data = {}
+        self.current = None
+
+    def line(self, linenr: int, line: str) -> None:
+        m = self._line_re.match(line)
+        if not m:
+            raise Deb822DecodeError(
+                f'Not a header, not a continuation at line {linenr + 1}')
+        elif cont := m.group('cont'):
+            self.data[self.current] += '\n' + cont
+        elif deb822_key := m.group('key'):
+            field = self.fields.get(deb822_key)
+            if not field and not self.ignore_unknown:
+                raise Deb822DecodeError(
+                    f'Unknown field "{deb822_key}" at line {linenr + 1}')
+
+            self.current = field
+            self.data[field] = m.group('value')
+        else:
+            raise NotImplementedError
+
+    def generate(self) -> _DataclassT | None:
+        if not self.data:
+            return None
+
+        r: dict[str, Any] = {}
+        for field, value in self.data.items():
+            field_factory: Optional[Callable[[str], Any]] = None
+            if field is None:
+                continue
+            elif (deb822_field := field.metadata.get('deb822')) and (load := deb822_field.load):
+                field_factory = load
+            elif isinstance(field.default_factory, type):
+                field_factory = field.default_factory
+            elif field.type in ('str', 'Optional[str]'):
+                field_factory = str
+            else:
+                raise RuntimeError(f'Unable to parse type {field.type}')
+
+            if field_factory is not None:
+                r[field.name] = field_factory(value)
+
+        self.reset()
+        return self.cls(**r)
+
+
+def read_deb822(
+    cls: type[_DataclassT],
+    file: IO[str],
+    /,
+    ignore_unknown: bool = False,
+) -> Iterable[_DataclassT]:
+    state = Deb822DecodeState(cls, ignore_unknown)
+
+    for linenr, line in enumerate(file):
+        line = line.rstrip('\n')
+
+        # Empty line, end of record
+        if line == '':
+            if (obj := state.generate()):
+                yield obj
+        # Strip comments rather than trying to preserve them
+        elif line[0] == '#':
+            continue
+        else:
+            state.line(linenr, line)
+
+    if (obj := state.generate()):
+        yield obj
+
+
+def write_deb822(
+    objs: Iterable[_DataclassT],
+    file: IO[str],
+    /,
+) -> None:
+    for obj in objs:
+        for field in dataclasses.fields(obj):
+            if (
+                (value := getattr(obj, field.name, None))
+                and (deb822_field := field.metadata.get('deb822'))
+                and (dump := deb822_field.dump) is not None
+            ):
+                folded = '\n '.join(dump(value).strip().split('\n'))
+                file.write(f'{deb822_field.key}: {folded}\n')
+        file.write('\n')

commit e4185c5e90f48e9170ac4d307650329ac5084a70

d/b/gencontrol.py: Fix generation of debian/tests/control
Currently we fail to include headers-kbuild tests in
debian/tests/control.

We read the template with read_deb822() which is a generator, but we
try to iterate over it twice which results in a an empty sequence the
second time.

Capture the generator output into a list so this works again.

Fixes: ae6ab60b1832 ("Convert control entries into dataclasses")

linux/debian/bin/gencontrol.py
@@ -443,13 +443,15 @@ linux-signed-{vars['arch']} (@signedtemplate_sourceversion@) {dist}; urgency={ur
             for package in packages_own:
                 package.build_profiles[0].neg.add('pkg.linux.quick')
 
-        tests_control_image = self.templates.get_tests_control('image.tests-control', vars)
+        tests_control_image = list(
+            self.templates.get_tests_control('image.tests-control', vars))
         for c in tests_control_image:
             c.depends.extend(
                 [i.name for i in packages_image_unsigned]
             )
 
-        tests_control_headers = self.templates.get_tests_control('headers.tests-control', vars)
+        tests_control_headers = list(
+            self.templates.get_tests_control('headers.tests-control', vars))
         for c in tests_control_headers:
             c.depends.extend(
                 [i.name for i in packages_headers] +